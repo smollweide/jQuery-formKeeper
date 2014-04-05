62c46db2bc669776237e8ae5fa9fb0565bda96a1
Simon Mollweide <simon.mollweide@namics.com>
2014-04-06 01:28:47 +0200

    Popover wrap required for optimal position

--- src/core/formkeeper.config.js
+++ src/core/formkeeper.config.js
@@ -11,8 +11,8 @@
 		classWrapSuccess: 'has-success',
 		classWrapError: 'has-error',
 		classPopover: 'form-keeper__popover',
-		templatePopover: '<div class="popover bottom form-keeper__popover">' +
-			'<div class="arrow"></div><p>{{title}}</p></div>',
+		templatePopover: '<div class="form-keeper__popover"><div class="popover bottom form-keeper__popover__inner">' +
+			'<div class="arrow"></div><p>{{title}}</p></div></div>',
 
 		bindAbleExtensions: []
 	};
